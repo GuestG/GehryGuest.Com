8dc3ecf19c359d9720d573c4777e67dbeb4e9fec
Gehry Guest <35783062+GuestG@users.noreply.github.com>
2021-11-17 15:21:15 -0800

    fixed contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,42 +19,46 @@ function Contact() {
                 Have a question or want to work together?
             </p>
             {/* <div className="contact-info"> */}
-                <form action="https://formsubmit.co/gehryguest@yahoo.com" method="POST" className="contact-info">
-                    {/* <input type="text" name="_honey" style="display:none">
-                    </input> */}
-                    
-                    <input type="text" id="name" className="name" placeholder="Your Name" required>
-                    </input>
+            <form action="https://formsubmit.co/e231169454c6ef1a337769280ec551af" method="POST" className="contact-info">
 
-                    <input type="email" id="email" className="email" placeholder="Your e-mail address" required>
-                    </input>
+                <input type="text" name="_honey" style="display: none;"/>
 
-                    <textarea type="text" name="textarea" id="message" className="message" placeholder="Type your message here.." required>
-                    </textarea>
+                    <input type="hidden" name="_captcha" value="false"/>
 
-                    <input type="hidden" name="_next" value="http://localhost:3000/">
-                    </input>
+                        <input type="hidden" name="_next" value="https://guestg.github.io/GehryGuestPortfolio/"/>
 
-                    <button type="submit" className="submit-message" id="submit-message">
-                        Submit
+                        <input type="text" name="name" id="name" className="name" placeholder="Your Name" required>
+                        </input>
+
+                        <input type="email" name="email" id="email" className="email" placeholder="Your e-mail address" required>
+                        </input>
+
+                        <textarea type="text" name="textarea" id="message" className="message" placeholder="Type your message here.." required>
+                        </textarea>
+
+                        <input type="hidden" name="_next" value="http://localhost:3000/">
+                        </input>
+
+                        <button type="submit" className="submit-message" id="submit-message">
+                            Submit
                     </button>
                 </form>
 
-            {/* </div> */}
+                    {/* </div> */}
 
 
 
-            <div className="external-links-wrapper">
-                <a className="githubIcon" href='https://github.com/GuestG' data-aos="fade-right">
-                    <FontAwesomeIcon icon={["fab", "github"]} />
-                </a>
-                <a className="linkedinIcon" href='https://linkedin.com/in/gehry-guest-903570177' data-aos="fade-left">
-                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
-                </a>
-            </div>
-            <p className="footers">
-                Gehry Guest <span className="my-name-color">©2021</span>
-            </p>
+                    <div className="external-links-wrapper">
+                        <a className="githubIcon" href='https://github.com/GuestG' data-aos="fade-right">
+                            <FontAwesomeIcon icon={["fab", "github"]} />
+                        </a>
+                        <a className="linkedinIcon" href='https://linkedin.com/in/gehry-guest-903570177' data-aos="fade-left">
+                            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                        </a>
+                    </div>
+                    <p className="footers">
+                        Gehry Guest <span className="my-name-color">©2021</span>
+                    </p>
         </div>
     )
 }
