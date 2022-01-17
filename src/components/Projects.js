8e00d04c6979c6cf7e3f5f936bd6ec67dca8f08b
Gehry Guest <35783062+GuestG@users.noreply.github.com>
2022-01-17 08:29:22 -0800

    fixed projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import DesignProject from "../assets/design.png";
+import GabisProject from "../assets/Gabis-Kids-Img.png";
 
 const Projects = () => {
     useEffect(() => {
@@ -18,59 +19,61 @@ const Projects = () => {
 
 
                 <div className="gallery-wrapper">
-                    {/* <div className="card" data-aos="fade-right">
-                        <img src={MagicProject} alt="" className="magic-project-img" />
+                    
+                    <div className="card" data-aos="fade-right">
+                        <img src={GabisProject} alt="" className="gabis-kids-img" />
                         <div className="text">
-                            <span className="text-highlight">React JS / API's</span>
+                            <span className="text-highlight">UX Design / Accessablity</span>
 
                         </div>
                         
-                        <button type="button" className="learn-more-button" id="mana-filter"
+                        <button type="button" className="learn-more-button" id="gabis-kids"
                             onClick={(e) => {
                             e.preventDefault();
-                            window.location.href='https://scryfall.com/';
+                            window.location.href='https://GabisKids.com/';
                         }}>
                             Learn More
                         </button>
                         
-                    </div> */}
+                    </div>
+                
                     <div className="card" data-aos="fade-left">
                         <img src={DesignProject} alt="" className="design-project-img" />
                         <div className="text">
                             <span className="text-highlight">Webpress / User Stories</span>
 
                         </div>
-                        
+
                         <button type="button" className="learn-more-button" id="maricellas-design-button"
                             onClick={(e) => {
-                            e.preventDefault();
-                            window.location.href='https://maricellasdesign.wordpress.com/';
-                        }}>
+                                e.preventDefault();
+                                window.location.href = 'https://maricellasdesign.wordpress.com/';
+                            }}>
                             Learn More
                         </button>
-                        
+
                     </div>
                 </div>
                 {/* <div className="project1" data-aos="fade-right">
-                            <h1 className="project-header">Mana Filter</h1>
-                            <img className="project1-img" />
-                            <p className="project-desc">This personal project of mine,
-                            I used a public API to query Magic the Gathering cards images.
-                            The application has users inputing text/name of the card, in order to retrieve card image from the 
-                            API's database.
+                    <h1 className="project-header">Mana Filter</h1>
+                    <img className="project1-img" />
+                    <p className="project-desc">This personal project of mine,
+                    I used a public API to query Magic the Gathering cards images.
+                    The application has users inputing text/name of the card, in order to retrieve card image from the
+                    API's database.
                             </p>
-                            <button className="project1-button" id="project1-button">
-                                Check it out
+                    <button className="project1-button" id="project1-button">
+                        Check it out
                             </button>
-                        </div>
-                        <div className="project2">
-                            <h1 className="project-header"></h1>
-                            <p className="project-desc">
+                </div>
+                <div className="project2">
+                    <h1 className="project-header"></h1>
+                    <p className="project-desc">
 
-                            </p>
-                        </div>
+                    </p>
+                </div>
 
-                        <div className="break"></div> */}
+                <div className="break"></div> */}
 
                 {/* <div className="project3">
                             <h1 className="project-header">Scryfall</h1>
